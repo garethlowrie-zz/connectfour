3658f3e76ef9f1e723f354ca39cd037c78581d63
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-10-09 11:23:00 +0100

    Render GameDisc component

--- src/components/RootGame/presentational.tsx
+++ src/components/RootGame/presentational.tsx
@@ -4,12 +4,13 @@ import Flex, { FlexItem } from 'styled-flex-component';
 import generateGame from 'utils/generateGame';
 import getDataForColumn from 'utils/getDataForColumn';
 import getNextSlotAvailableInColumn from 'utils/getNextSlotAvailableInColumn';
-
+import GameDisc from 'components/GameDisc/presentational';
+import Teams from 'enums/Teams';
 interface IPropTypes {
 
 }
 
-const DATA = [{ "x": 0, "y": 0 }, { "x": 1, "y": 0 }, { "x": 2, "y": 0 }, { "x": 3, "y": 0 }, { "x": 4, "y": 0 }, { "x": 5, "y": 0 }, { "x": 6, "y": 0 }, { "x": 0, "y": 1 }, { "x": 1, "y": 1 }, { "x": 2, "y": 1 }, { "x": 3, "y": 1 }, { "x": 4, "y": 1 }, { "x": 5, "y": 1 }, { "x": 6, "y": 1 }, { "x": 0, "y": 2 }, { "x": 1, "y": 2 }, { "x": 2, "y": 2 }, { "x": 3, "y": 2 }, { "x": 4, "y": 2 }, { "x": 5, "y": 2 }, { "x": 6, "y": 2 }, { "x": 0, "y": 3 }, { "x": 1, "y": 3 }, { "x": 2, "y": 3 }, { "x": 3, "y": 3 }, { "x": 4, "y": 3 }, { "x": 5, "y": 3 }, { "x": 6, "y": 3 }, { "x": 0, "y": 4 }, { "x": 1, "y": 4 }, { "x": 2, "y": 4 }, { "x": 3, "y": 4 }, { "x": 4, "y": 4 }, { "x": 5, "y": 4 }, { "x": 6, "y": 4 }, { "x": 0, "y": 5 }, { "x": 1, "y": 5 }, { "x": 2, "y": 5 }, { "x": 3, "y": 5 }, { "x": 4, "y": 5 }, { "x": 5, "y": 5 }, { "x": 6, "y": 5 }];
+const DATA = [{ "x": 0, "y": 0, color: Teams.Red }, { "x": 1, "y": 0, color: Teams.Red }, { "x": 2, "y": 0, color: Teams.Yellow }, { "x": 3, "y": 0, color: Teams.Yellow }, { "x": 4, "y": 0, color: Teams.Red }, { "x": 5, "y": 0, color: Teams.Yellow }, { "x": 6, "y": 0, color: Teams.Red }, { "x": 0, "y": 1 }, { "x": 1, "y": 1, color: Teams.Red }, { "x": 2, "y": 1, color: Teams.Yellow }, { "x": 3, "y": 1, color: Teams.Yellow }, { "x": 4, "y": 1, color: Teams.Red }, { "x": 5, "y": 1, color: Teams.Red }, { "x": 6, "y": 1, color: Teams.Red }, { "x": 0, "y": 2 }, { "x": 1, "y": 2, color: Teams.Red }, { "x": 2, "y": 2, color: Teams.Red }, { "x": 3, "y": 2, color: Teams.Red }, { "x": 4, "y": 2, color: Teams.Yellow }, { "x": 5, "y": 2, color: Teams.Yellow }, { "x": 6, "y": 2, color: Teams.Yellow }, { "x": 0, "y": 3 }, { "x": 1, "y": 3, color: Teams.Red }, { "x": 2, "y": 3, color: Teams.Red }, { "x": 3, "y": 3, color: Teams.Red }, { "x": 4, "y": 3, color: Teams.Red }, { "x": 5, "y": 3, color: Teams.Yellow }, { "x": 6, "y": 3 }, { "x": 0, "y": 4 }, { "x": 1, "y": 4 }, { "x": 2, "y": 4 }, { "x": 3, "y": 4 }, { "x": 4, "y": 4 }, { "x": 5, "y": 4 }, { "x": 6, "y": 4 }, { "x": 0, "y": 5 }, { "x": 1, "y": 5 }, { "x": 2, "y": 5 }, { "x": 3, "y": 5 }, { "x": 4, "y": 5 }, { "x": 5, "y": 5 }, { "x": 6, "y": 5 }];
 
 const RootGame: React.SFC<IPropTypes> = ({
 
@@ -22,7 +23,7 @@ const RootGame: React.SFC<IPropTypes> = ({
 	return (
 		<Flex column justifyCenter alignCenter className={styles.container}>
 			<FlexItem>
-
+				<GameDisc variant={Teams.Yellow} /><GameDisc variant={Teams.Red} />
 			</FlexItem>
 
 		</Flex>
