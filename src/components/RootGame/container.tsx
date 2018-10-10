681b88f17b27d6c557e5493ed61b64feadf2c492
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-10-10 14:14:17 +0100

    Rename of onClick function to prevent conflict with react-pose overwriting func args

--- src/components/RootGame/container.tsx
+++ src/components/RootGame/container.tsx
@@ -9,26 +9,25 @@ import generateColumns from 'utils/generateColumns';
 import withPropsOnChange from 'recompose/withPropsOnChange';
 import findIndex from 'lodash-es/findIndex';
 
-
 export default compose(
 	withPropsOnChange(['data'], ({ data }: any) => ({
-		columns: generateColumns(data, MAX_COLUMNS)
+			columns: generateColumns(data, MAX_COLUMNS)
 	})),
 	withHandlers({
-		onColumnClick: ({ data, activeTeam, onClick }: any) => (column: number) => {
+		onColumnClick: ({ data, activeTeam, onPlayerTakesTurn }: any) => (column: number) => {
 			const colData = getColumn(data, column);
 			const nextSlot = getNextSlotAvailableInColumn(colData);
 			let winner;
-
 			if (nextSlot) {
 				const itemToUpdate = findIndex(data, { x: nextSlot.x, y: nextSlot.y }); // Find the index of item to update
 				const newItem = data[itemToUpdate];										// Find the item to update
 				newItem.color = activeTeam;												// Add teams color to item
 				data[itemToUpdate] = newItem;											// Amend the item in data
 				winner = checkForWin2(data, activeTeam);
-				onClick(data, winner);
+
+				onPlayerTakesTurn(data, winner);
 			}
 
 		}
-	})
+	}),
 )(RootGame);
